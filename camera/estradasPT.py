426daa6d3d365771633b49741666129d03fe1dd8
dpjrodrigues <dpjrodrigues@gmail.com>
2019-02-22 11:35:59 +0000

    async_update

--- camera/estradasPT.py
+++ camera/estradasPT.py
@@ -5,9 +5,10 @@ Based on https://home-assistant.io/components/camera.ffmpeg/
 import asyncio
 import logging
 import async_timeout
-
 import voluptuous as vol
 
+from datetime import datetime, timedelta
+from homeassistant.util import Throttle
 from homeassistant.const import CONF_NAME
 from homeassistant.components.camera import Camera, PLATFORM_SCHEMA
 from homeassistant.components.ffmpeg import (
@@ -15,6 +16,8 @@ from homeassistant.components.ffmpeg import (
 import homeassistant.helpers.config_validation as cv
 from homeassistant.helpers.aiohttp_client import (
     async_aiohttp_proxy_stream, async_get_clientsession)
+from pyEstradasPT import Cameras
+
 
 _LOGGER = logging.getLogger(__name__)
 
@@ -28,11 +31,12 @@ PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Optional(CONF_NAME, default=DEFAULT_NAME): cv.string,
 })
 
+SCAN_INTERVAL = timedelta(minutes=5)
+
 
 async def async_setup_platform(hass, config, async_add_entities,
                                discovery_info=None):
     """Set up a FFmpeg camera."""
-    from pyEstradasPT import Cameras
 
     websession = async_get_clientsession(hass)
 #    with async_timeout.timeout(10, loop=hass.loop):
@@ -51,10 +55,15 @@ class FFmpegCamera(Camera):
         """Initialize a FFmpeg camera."""
         super().__init__()
 
+        self._hass = hass
         self._manager = hass.data[DATA_FFMPEG]
         self._name = config.get(CONF_NAME)
+        self._cam_name = config.get(CONF_INPUT)
         self._input = url
+        self._token_changed = True
         self._extra_arguments = config.get(CONF_EXTRA_ARGUMENTS)
+        self._last_update = datetime.now()
+
 
     async def async_camera_image(self):
         """Return a still image response from the camera."""
@@ -90,6 +99,28 @@ class FFmpegCamera(Camera):
     def device_state_attributes(self):
         """Return other details about the sensor state."""
         attrs = {}
+        attrs["camera_name"] = self._cam_name
+        attrs["last_update"] = self._last_update
+        attrs["token_changed"] = self._token_changed
         attrs["url"] = self._input
         return attrs 
 
+    @Throttle(SCAN_INTERVAL)
+    async def async_update(self):
+        """Update the cam."""
+
+        websession = async_get_clientsession(self._hass)
+        #    with async_timeout.timeout(10, loop=hass.loop):
+        cams = await Cameras.get(websession) 
+       
+        url = await cams.UrlByCameraName(self._cam_name)
+        url = url.replace(".3gp?token=", ".webm?token=")
+
+        if url == self._input:
+            self._token_changed = False
+        else:
+            self._token_changed = True
+            self._input = url
+
+        self._last_update = datetime.now()
+        self.schedule_update_ha_state()
