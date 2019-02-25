1d4a3bab7fd4fd28ded2d3e15af89a785661bdc4
dpjrodrigues <dpjrodrigues@gmail.com>
2019-02-25 16:22:08 +0000

    super() implementation

--- estradas_pt/camera.py
+++ estradas_pt/camera.py
@@ -10,7 +10,7 @@ import voluptuous as vol
 from datetime import datetime, timedelta
 from homeassistant.util import Throttle
 from homeassistant.const import CONF_NAME
-from homeassistant.components.camera import Camera, PLATFORM_SCHEMA
+from homeassistant.components.camera.ffmpeg import FFmpegCamera, PLATFORM_SCHEMA
 from homeassistant.components.ffmpeg import (
     DATA_FFMPEG, CONF_INPUT, CONF_EXTRA_ARGUMENTS)
 import homeassistant.helpers.config_validation as cv
@@ -39,21 +39,19 @@ async def async_setup_platform(hass, config, async_add_entities,
     websession = async_get_clientsession(hass)
 
     from pyEstradasPT import Cameras
-#    with async_timeout.timeout(10, loop=hass.loop):
+    #with async_timeout.timeout(10, loop=hass.loop):
     cams = await Cameras.get(websession) 
        
     url = await cams.UrlByCameraName(config.get(CONF_INPUT))      
+    
+    async_add_entities([EstradasCamera(hass, config, url.replace(".3gp?token=", ".webm?token="))])
 
-    async_add_entities([FFmpegCamera(hass, config, url.replace(".3gp?token=", ".webm?token="))])
-
-
-
-class FFmpegCamera(Camera):
+class EstradasCamera(FFmpegCamera):
     """An implementation of an FFmpeg camera."""
 
     def __init__(self, hass, config, url):
-        """Initialize a FFmpeg camera."""
-        super().__init__()
+        """Initialize a Estradas camera."""
+        super().__init__(hass,config)
 
         self._manager = hass.data[DATA_FFMPEG]
         self._name = config.get(CONF_NAME)
@@ -63,38 +61,11 @@ class FFmpegCamera(Camera):
         self._extra_arguments = config.get(CONF_EXTRA_ARGUMENTS)
         self._last_update = datetime.now()
 
-
-    async def async_camera_image(self):
-        """Return a still image response from the camera."""
-        from haffmpeg import ImageFrame, IMAGE_JPEG
-        ffmpeg = ImageFrame(self._manager.binary, loop=self.hass.loop)
-
-        image = await asyncio.shield(ffmpeg.get_image(
-            self._input, output_format=IMAGE_JPEG,
-            extra_cmd=self._extra_arguments), loop=self.hass.loop)
-        return image
-
     async def handle_async_mjpeg_stream(self, request):
         """Generate an HTTP MJPEG stream from the camera."""
-        from haffmpeg import CameraMjpeg
-
         await self.async_update()
-        stream = CameraMjpeg(self._manager.binary, loop=self.hass.loop)
-        await stream.open_camera(
-            self._input, extra_cmd=self._extra_arguments)
+        await super().handle_async_mjpeg_stream(request)
 
-        try:
-            return await async_aiohttp_proxy_stream(
-                self.hass, request, stream,
-                self._manager.ffmpeg_stream_content_type)
-        finally:
-            await stream.close()
-
-    @property
-    def name(self):
-        """Return the name of this camera."""
-        return self._name
-    
     @property
     def device_state_attributes(self):
         """Return other details about the sensor state."""
@@ -112,7 +83,7 @@ class FFmpegCamera(Camera):
         websession = async_get_clientsession(self.hass)
         
         from pyEstradasPT import Cameras
-        #    with async_timeout.timeout(10, loop=hass.loop):
+        #with async_timeout.timeout(10, loop=hass.loop):
         cams = await Cameras.get(websession) 
        
         url = await cams.UrlByCameraName(self._cam_name)
